ef81013a125c3e7110e17a0e46746beb554c4914
hassanmehmood277 <hassanmehmood812@gmail.com>
2022-04-06 10:12:10 +0500

    Day 1 activity

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function func() {
   document.getElementById("demo").innerHTML = "Text Changed";
+  x = document.getElementById("demo");
+  x.style.fontSize = "100px";
+  x.style.backgroundColor = "yellow";
+  x.style.color = "red";
 }
-
